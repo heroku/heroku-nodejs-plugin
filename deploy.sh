8a0aa3f30c6eab170932d502708441fd64c7aff8
Jeremy Morrell <jmorrell@heroku.com>
2018-06-25 07:03:38 -0700

    debug

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,24 @@ TRAVIS_TAG="latest"
 
 # if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ -n $TRAVIS_TAG ]]; then
 echo $(ls .)
+echo $(pwd)
 
-ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar"
+# Name the tarball
+ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar.gz"
 
 echo "archive name: $ARCHIVE_NAME"
 
 echo "build dir: $TRAVIS_BUILD_DIR"
 
-tar --create --verbose --file="$ARCHIVE_NAME" --directory "$TRAVIS_BUILD_DIR/dist"
+# Compress the built directory into a tarball
+tar -czf --file="$ARCHIVE_NAME" --directory "./dist"
 
 echo "Successfully created tar"    
 echo $(ls .)
+
+# Generate a SHA and save that
+
+# Publish to github releases
 
 # else
 #     echo "Skipping deploy because event type is: $TRAVIS_EVENT_TYPE and git tag is: $TRAVIS_TAG"
