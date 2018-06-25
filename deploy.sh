dd5dd32d5af893840ff37cd9e863182e847c548d
Jeremy Morrell <jmorrell@heroku.com>
2018-06-25 05:15:15 -0700

    build on install

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+set -o errexit    # always exit on error
+set -o pipefail   # don't ignore exit codes when piping output
+
 echo $(ls dist)
 echo $TRAVIS_TAG
 echo $TRAVIS_NODE_VERSION
+echo $TRAVIS_EVENT_TYPE
+echo $TRAVIS_COMMIT_MESSAGE
 
 
 # - ARCHIVE_NAME="$TRAVIS_NODE_VERSION-${TRAVIS_TAG:-latest}-$TRAVIS_OS_NAME-`uname -m`.tar"
