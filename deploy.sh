eac80b5db851ae63557bd393b7b7fa3a19f91fe0
Jeremy Morrell <jmorrell@heroku.com>
2018-06-25 07:47:39 -0700

    debug

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,12 @@ set -o pipefail   # don't ignore exit codes when piping output
 TRAVIS_TAG="latest"
 
 # if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ -n $TRAVIS_TAG ]]; then
-echo $(ls .)
-echo $(pwd)
 
 # Name the tarball
 ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar.gz"
+ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.sha1"
 
-echo "archive name: $ARCHIVE_NAME"
-
-echo $(ls $TRAVIS_BUILD_DIR/dist)
+echo "Saving build as $ARCHIVE_NAME"
 
 # Compress the built directory into a tarball
 tar -czf $ARCHIVE_NAME dist/
@@ -23,6 +20,9 @@ tar -czf $ARCHIVE_NAME dist/
 echo "Successfully created tar"    
 
 # Generate a SHA and save that
+sha1sum $ARCHIVE_NAME > $ARCHIVE_SHA_NAME
+
+cat $ARCHIVE_SHA_NAME
 
 # Publish to github releases
 
