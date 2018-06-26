17eeeb4b3c4380a2d04414c20226ccf31225321a
Jeremy Morrell <morrell.jeremy@gmail.com>
2018-06-26 16:39:08 -0700

    Add publish script (#2)

--- build.sh
+++ build.sh
@@ -3,16 +3,18 @@
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
+echo "Building plugin"
+
 # delete /build and /dist if it exists
 rm -rf build dist
 
 # run the build using node-gyp
-./node_modules/.bin/node-gyp configure build
+npx node-gyp configure build
 
 # roll the javascript into one file
-./node_modules/.bin/webpack-cli
+npx webpack-cli
 
 # copy over the README
 cp ./src/README.md ./dist/README.md
 
-echo $(ls ./dist)
+echo "Successfully built plugin"
--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,31 @@ set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
 if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ -n $TRAVIS_TAG ]]; then
-    ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar"
-    tar --create --verbose --file="$ARCHIVE_NAME" --directory "$TRAVIS_BUILD_DIR/dist"
+    # Name the tarball
+    ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar.gz"
+    ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.sha512"
+
+    echo "Saving build as $ARCHIVE_NAME"
+
+    # Compress the built directory into a tarball
+    tar -czf $ARCHIVE_NAME dist/
+    # Generate a SHA and save that
+    sha512sum $ARCHIVE_NAME > $ARCHIVE_SHA_NAME
 
     echo "Successfully created tar"    
-    echo $(ls .)
+
+    # Publish to github releases
+
+    echo "Publishing binary"    
+
+    npx github-release upload \
+    --owner heroku \
+    --repo heroku-nodejs-plugin \
+    --token $GITHUB_TOKEN \
+    --tag $TRAVIS_TAG \
+    --name $TRAVIS_TAG \
+    --body $TRAVIS_TAG \
+    $ARCHIVE_NAME $ARCHIVE_SHA_NAME
 else
     echo "Skipping deploy because event type is: $TRAVIS_EVENT_TYPE and git tag is: $TRAVIS_TAG"
 fi
