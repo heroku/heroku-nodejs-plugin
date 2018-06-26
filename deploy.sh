a4297b4c457dff74a4df081ca21bd8b851e34e80
Jeremy Morrell <jmorrell@heroku.com>
2018-06-26 15:25:22 -0700

    debug

--- deploy.sh
+++ deploy.sh
@@ -3,38 +3,33 @@
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
-# override this for testing while developing
-TRAVIS_TAG="testing"
+if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ -n $TRAVIS_TAG ]]; then
+    # Name the tarball
+    ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar.gz"
+    ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.sha512"
 
-# if [[ $TRAVIS_EVENT_TYPE == "push" ]] && [[ -n $TRAVIS_TAG ]]; then
+    echo "Saving build as $ARCHIVE_NAME"
 
-# Name the tarball
-ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.tar.gz"
-ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$TRAVIS_TAG.sha512"
+    # Compress the built directory into a tarball
+    tar -czf $ARCHIVE_NAME dist/
+    # Generate a SHA and save that
+    sha512sum $ARCHIVE_NAME > $ARCHIVE_SHA_NAME
 
-echo "Saving build as $ARCHIVE_NAME"
+    echo "Successfully created tar"    
 
-# Compress the built directory into a tarball
-tar -czf $ARCHIVE_NAME dist/
+    # Publish to github releases
 
-echo "Successfully created tar"    
+    echo "Publishing binary"    
 
-# Generate a SHA and save that
-sha512sum $ARCHIVE_NAME > $ARCHIVE_SHA_NAME
-
-# Publish to github releases
-
-npx github-release upload \
-  --owner heroku \
-  --repo heroku-nodejs-plugin \
-  --token $GITHUB_TOKEN \
-  --tag $TRAVIS_TAG \
-  --name $TRAVIS_TAG \
-  --body $TRAVIS_TAG \
-  $ARCHIVE_NAME $ARCHIVE_SHA_NAME
-
-
-# else
-#     echo "Skipping deploy because event type is: $TRAVIS_EVENT_TYPE and git tag is: $TRAVIS_TAG"
-# fi
+    npx github-release upload \
+    --owner heroku \
+    --repo heroku-nodejs-plugin \
+    --token $GITHUB_TOKEN \
+    --tag $TRAVIS_TAG \
+    --name $TRAVIS_TAG \
+    --body $TRAVIS_TAG \
+    $ARCHIVE_NAME $ARCHIVE_SHA_NAME
+else
+    echo "Skipping deploy because event type is: $TRAVIS_EVENT_TYPE and git tag is: $TRAVIS_TAG"
+fi
 
