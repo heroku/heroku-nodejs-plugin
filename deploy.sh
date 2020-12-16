b3b277430966c84ea083ccdf1797dcbe482bad2e
Danielle Adams <danielle.adams@heroku.com>
2020-12-16 16:25:54 -0500

    add deploy script to Circle config

--- deploy.sh
+++ deploy.sh
@@ -3,32 +3,28 @@
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
+NODE_VERSION=$1
+
 if [[ $CIRCLE_BRANCH == "main" ]] && [[ -n $CIRCLE_TAG ]]; then
     # Name the tarball
-    ARCHIVE_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$CIRCLE_TAG.tar.gz"
-    ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$TRAVIS_NODE_VERSION-$CIRCLE_TAG.sha512"
+    ARCHIVE_NAME="heroku-nodejs-plugin-node-$NODE_VERSION-$CIRCLE_TAG.tar.gz"
+    ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$NODE_VERSION-$CIRCLE_TAG.sha512"
 
     echo "Saving build as $ARCHIVE_NAME"
 
     # Compress the built directory into a tarball
-    tar -czf $ARCHIVE_NAME heroku-nodejs-plugin/
+    tar -czf $ARCHIVE_NAME "heroku-nodejs-plugin-$NODE_VERSION/"
     # Generate a SHA and save that
     sha512sum $ARCHIVE_NAME > $ARCHIVE_SHA_NAME
 
     echo "Successfully created tar"    
 
+    echo "Publishing binary"
+
     # Publish to github releases
+    node ./scripts/release.js $ARCHIVE_NAME $ARCHIVE_SHA_NAME
 
-    echo "Publishing binary"    
-
-    npx github-release upload \
-    --owner heroku \
-    --repo heroku-nodejs-plugin \
-    --token $GITHUB_TOKEN \
-    --tag $TRAVIS_TAG \
-    --name $TRAVIS_TAG \
-    --body $TRAVIS_TAG \
-    $ARCHIVE_NAME $ARCHIVE_SHA_NAME
+    echo "Successfully published released"
 else
     echo "Skipping deploy because branch is: $CIRCLE_BRANCH; and git tag is: $CIRCLE_TAG"
 fi
