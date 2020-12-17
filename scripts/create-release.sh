7d97d533b1327b2186852262a1c7afe510236c43
Danielle Adams <danielle.adams@heroku.com>
2020-12-17 14:36:02 -0500

    remove node parameter to creat-github-release job

--- scripts/create-release.sh
+++ scripts/create-release.sh
@@ -3,13 +3,7 @@
 set -o errexit    # always exit on error
 set -o pipefail   # don't ignore exit codes when piping output
 
-NODE_VERSION=$1
-
 if [[ $CIRCLE_PROJECT_USERNAME == "heroku" ]] && [[ -n $CIRCLE_TAG ]]; then
-    # Name the tarball
-    ARCHIVE_NAME="heroku-nodejs-plugin-node-$NODE_VERSION-$CIRCLE_TAG.tar.gz"
-    ARCHIVE_SHA_NAME="heroku-nodejs-plugin-node-$NODE_VERSION-$CIRCLE_TAG.sha512"
-
     echo "Creating GitHub release"
 
     node ./scripts/create-release.js
